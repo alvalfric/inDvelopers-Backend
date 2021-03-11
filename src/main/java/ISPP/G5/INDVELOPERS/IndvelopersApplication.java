cd85f94fff5d5bbc1c5dd534df7d0431b0aca54c
Ferwalrod <ferwalrod@alum.us.es>
2021-03-11 17:10:25 +0100

    inicio del repositorio en back-end

---
+++ src/main/java/ISPP/G5/INDVELOPERS/IndvelopersApplication.java
@@ -0,0 +1,13 @@
+package ISPP.G5.INDVELOPERS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndvelopersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndvelopersApplication.class, args);
+	}
+
+}
