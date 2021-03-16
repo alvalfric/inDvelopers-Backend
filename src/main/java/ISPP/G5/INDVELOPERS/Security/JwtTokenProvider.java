9bb48f37a56aec0cce40bfc0307dc9f88044bb0c
Ferwalrod <ferwalrod@alum.us.es>
2021-03-16 14:40:44 +0100

    Seguridad añadida

---
+++ src/main/java/ISPP/G5/INDVELOPERS/Security/JwtTokenProvider.java
@@ -0,0 +1,88 @@
+package ISPP.G5.INDVELOPERS.Security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import ISPP.G5.INDVELOPERS.models.UserRole;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtTokenProvider {
+
+  /**
+   * THIS IS NOT A SECURE PRACTICE! For simplicity, we are storing a static key here. Ideally, in a
+   * microservices environment, this key would be kept on a config-server.
+   */
+  @Value("${security.jwt.token.secret-key}")
+  private String secretKey;
+
+  @Value("${security.jwt.token.expire-length}")
+  private long validityInMilliseconds; // 1h
+
+  @Autowired
+  private UserDetailsServiceImpl myUserDetails;
+
+  @PostConstruct
+  protected void init() {
+    secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+  }
+
+  public String createToken(String username, String userId, Set<UserRole> roles) {
+
+    Claims claims = Jwts.claims().setSubject(username);
+    claims.put("userId", userId);
+    claims.put("auth", roles.stream().map(ur -> new SimpleGrantedAuthority("ROLE_"+ur.name())).collect(Collectors.toList()));
+
+    Date now = new Date();
+    Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+    return Jwts.builder()//
+        .setClaims(claims)//
+        .setIssuedAt(now)//
+        .setExpiration(validity)//
+        .signWith(SignatureAlgorithm.HS256, secretKey)//
+        .compact();
+  }
+
+  public Authentication getAuthentication(String token) {
+    UserDetails userDetails = myUserDetails.loadUserByUsername(getUsername(token));
+    return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+  }
+
+  public String getUsername(String token) {
+    return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+  }
+
+  public String resolveToken(HttpServletRequest req) {
+    String bearerToken = req.getHeader("Authorization");
+    if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+      return bearerToken.substring(7);
+    }
+    return null;
+  }
+
+  public boolean validateToken(String token) {
+    try {
+      Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+      return true;
+    } catch (JwtException | IllegalArgumentException e) {
+      throw new IllegalArgumentException("Expired or invalid JWT token");
+    }
+  }
+
+}
