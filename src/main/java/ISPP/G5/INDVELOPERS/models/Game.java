ffe15d645edf14a16a3cf18b119fb6d4418485a3
David <davcacrom@alum.us.es>
2021-04-04 13:11:01 +0100

    Described posibilities for insert images in games.

--- src/main/java/ISPP/G5/INDVELOPERS/models/Game.java
+++ src/main/java/ISPP/G5/INDVELOPERS/models/Game.java
@@ -16,28 +16,35 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-@Document(collection="Game")
+@Document(collection = "Game")
 @AllArgsConstructor
 @NoArgsConstructor
-public class Game extends BaseEntity{
+public class Game extends BaseEntity {
 
 	@NotBlank
 	private String title;
-	
+
 	@NotBlank
 	private String description;
-	
+
 	@NotBlank
 	private String requirements;
-	
+
 	@NotNull
 	private Double price;
-	
+
 	private String idCloud;
-	
+
 	private Boolean isNotMalware;
-	
+
 	private Developer creator;
+
+	// Posibilidad 1: Usar image como atributo de game nomal.
+
+	// Posibilidad 2: Usar image como atributo de game pero que al crear un game sea
+	// null y después, en una pantalla aparte y con su propio controlador (tipo /games/id/image/add) se le añada la portada.
 	
-    private Binary image;
+	// Posibilidad 3: Añadir image como una nueva clase (tipo GameImage o algo asi)
+
+	private Binary image;
 }
