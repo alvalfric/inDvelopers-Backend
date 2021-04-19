007f7e85a79b04935aed263741e5fb868f4f3b1a
Alvaro <alvalfric@alum.us.es>
2021-04-19 20:43:21 +0200

    Updated file size

--- src/main/java/ISPP/G5/INDVELOPERS/paypal/PaypalController.java
+++ src/main/java/ISPP/G5/INDVELOPERS/paypal/PaypalController.java
@@ -62,8 +62,8 @@ public class PaypalController {
 	@Autowired
 	private DeveloperSubscriptionService devSubscrService;
 
-	public static final String MAIN_PAGE_DEPLOYED_URL = "https://continuous-indvelopers.herokuapp.com";
-//	public static final String MAIN_PAGE_DEPLOYED_URL = "https://level2-indvelopers.herokuapp.com";
+//	public static final String MAIN_PAGE_DEPLOYED_URL = "https://continuous-indvelopers.herokuapp.com";
+	public static final String MAIN_PAGE_DEPLOYED_URL = "https://level2-indvelopers.herokuapp.com";
 //	public static final String MAIN_PAGE_DEPLOYED_URL = "http://localhost:3000";
 
 	public static final String SUCCESS_URL = "/success";
